2d043f1a20b7479cb563b8aa08bc916314b24983
icyblob <dignity.rashes_0w@icloud.com>
2025-03-31 19:14:04 -0400

    Update environment_setup.sh
    The script will stop if the existing VBox is not 7.1.4. Continue otherwise.

--- environment_setup.sh
+++ environment_setup.sh
@@ -41,18 +41,32 @@ apt install -y freerdp2-x11 git cmake docker.io libxcb-cursor0 sshpass gcc-12 g+
 # Create mount point
 mkdir /mnt/qubic
 
-# Download and install VirtualBox
-wget https://download.virtualbox.org/virtualbox/7.1.4/virtualbox-7.1_7.1.4-165100~Ubuntu~jammy_amd64.deb
-wget https://download.virtualbox.org/virtualbox/7.1.4/Oracle_VirtualBox_Extension_Pack-7.1.4.vbox-extpack
-dpkg -i virtualbox-7.1_7.1.4-165100~Ubuntu~jammy_amd64.deb
-apt --fix-broken install -y
+# Check if VirtualBox is installed and its version
+DESIRED_VBOX_VERSION="7.1.4"
+VBOX_INSTALLED_VERSION=$(VBoxManage --version 2>/dev/null | cut -d 'r' -f 1)
 
-# Install VirtualBox Extension Pack with auto license acceptance
-VBoxManage extpack install Oracle_VirtualBox_Extension_Pack-7.1.4.vbox-extpack --accept-license=eb31505e56e9b4d0fbca139104da41ac6f6b98f8e78968bdf01b1f3da3c4f9ae
+if [ -n "$VBOX_INSTALLED_VERSION" ]; then
+    if [ "$VBOX_INSTALLED_VERSION" == "$DESIRED_VBOX_VERSION" ]; then
+        echo "VirtualBox version $DESIRED_VBOX_VERSION is already installed. Skipping installation."
+    else
+        echo "VirtualBox version $VBOX_INSTALLED_VERSION is installed, but version $DESIRED_VBOX_VERSION is required."
+        echo "Please uninstall the current version of VirtualBox and rerun this script."
+        exit 1
+    fi
+else
+    # Download and install VirtualBox
+    wget https://download.virtualbox.org/virtualbox/7.1.4/virtualbox-7.1_7.1.4-165100~Ubuntu~jammy_amd64.deb
+    wget https://download.virtualbox.org/virtualbox/7.1.4/Oracle_VirtualBox_Extension_Pack-7.1.4.vbox-extpack
+    dpkg -i virtualbox-7.1_7.1.4-165100~Ubuntu~jammy_amd64.deb
+    apt --fix-broken install -y
 
-# Remove VirtualBox kernel modules and reconfigure
-modprobe -r vboxnetflt vboxnetadp vboxpci vboxdrv
-/sbin/vboxconfig
+    # Install VirtualBox Extension Pack with auto license acceptance
+    VBoxManage extpack install Oracle_VirtualBox_Extension_Pack-7.1.4.vbox-extpack --accept-license=eb31505e56e9b4d0fbca139104da41ac6f6b98f8e78968bdf01b1f3da3c4f9ae
+
+    # Remove VirtualBox kernel modules and reconfigure
+    modprobe -r vboxnetflt vboxnetadp vboxpci vboxdrv
+    /sbin/vboxconfig
+fi
 
 # Install Docker Compose
 curl -L "https://github.com/docker/compose/releases/download/v2.26.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
